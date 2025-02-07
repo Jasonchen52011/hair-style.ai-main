98e3def2e719be28b6887e9a411a6b34169daedc
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-02-08 00:00:32 +0800

    feat

--- app/metadata.ts
+++ app/metadata.ts
@@ -70,9 +70,11 @@ const baseMetadata: Metadata = {
     robots: {
         index: true,
         follow: true,
+        nocache: false,
         googleBot: {
             index: true,
             follow: true,
+            noimageindex: false,
             'max-video-preview': -1,
             'max-image-preview': 'large',
             'max-snippet': -1,
