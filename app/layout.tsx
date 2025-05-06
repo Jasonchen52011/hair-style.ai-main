95c31959f30c4fc630f967ad91759a37ee4c9bd8
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-05-06 21:25:04 +0800

    “fig”

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,17 @@ export default function RootLayout({
   return (
     <html>
       <head>
+      <script
+        id="clarity-script"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "r341ayxao1");`
+        }}
+      />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
