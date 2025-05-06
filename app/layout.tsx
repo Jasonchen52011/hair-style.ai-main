63c6cef0ad66df70cd0d5bec01fc3f4f1079f9f8
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-05-06 21:09:19 +0800

    “fig”

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({
   return (
     <html>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
