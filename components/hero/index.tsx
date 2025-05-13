3a33cba1dcb9a98ecf9fecc5c962bbfddb850b78
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-05-13 15:00:12 +0800

    “fig”

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -245,12 +245,13 @@ export default function Hero() {
                         </h1>
                         
                         <p className="text-lg text-gray-600 mb-4">
-                            Not sure which hairstyle suits you best? Our free AI hairstyles changer lets you try on <span className=" font-bold text-purple-600">60+ styles</span> in just a few clicks!
+                            Not sure which hairstyle suits you best? Our free AI hairstyles changer lets you try on <span className=" font-bold 
+                            ">60+ styles</span> in just a few clicks!
                             Whether you want <span className=" font-bold">short, curly, wavy, or bold styles like buzz cuts and braids,</span>
                              this free AI simulator haircut tool helps you experiment without a trip to the salon.</p>
                              
                              <p className="text-lg text-gray-600 mb-4 leading-relaxed">
-                             Whether you need an online hairstyles changer for <span className=" font-bold text-purple-600">men or women,</span> 
+                             Whether you need an online hairstyles changer for <span className=" font-bold">men or women,</span> 
                              this tool has it all.Hairstyle try-on has never been easier – upload your photo and explore the best styles.
                         </p>
 
