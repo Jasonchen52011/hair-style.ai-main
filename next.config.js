9758133ca060008c7aa9f3fa864e7283518180b7
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-06-15 18:43:36 +0800

    “fix”

--- next.config.js
+++ next.config.js
@@ -113,7 +113,6 @@ const nextConfig = {
         unoptimized: true, // 启用Next.js图片优化
         formats: ['image/webp', 'image/avif'], // 优先使用现代格式
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         remotePatterns: [
             {
                 protocol: 'https',
