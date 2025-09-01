82df3036ac05e363925741f6b524e97abde1106e
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-09-01 12:19:51 +0800

    fix

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,12 +1,9 @@
-import Script from 'next/script';
-
 export default function GoogleAdSense() {
   return (
-    <Script
+    <script
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2318931889728296"
       crossOrigin="anonymous"
-      strategy="afterInteractive"
-    />
+    ></script>
   );
 }
