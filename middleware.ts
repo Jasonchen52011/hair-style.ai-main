ae135f45ca7f435b6546f215539c894ad47ee7cf
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-11-12 00:14:53 +0700

    Fix middleware matcher to exclude fonts and data files

--- middleware.ts
+++ middleware.ts
@@ -49,10 +49,12 @@ export const config = {
      * Match all request paths except:
      * - _next/static (static files)
      * - _next/image (image optimization files)
+     * - _next/data (data files)
      * - favicon.ico (favicon file)
+     * - fonts - .woff, .woff2, .ttf, .otf
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|_next/data|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|woff|woff2|ttf|otf)$).*)',
   ],
 }
